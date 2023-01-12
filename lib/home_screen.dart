b625e855191a7fc9fd46578c205143b713e3ff65
mercyjae <famakinwamercy20@gmail.com>
2023-01-12 09:37:51 -0800

    feat: implement login UI

---
+++ lib/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('Girls that code'),
+          Text('Girls that code'),
+          Text('Girls that code'),
+          Text('Girls that code'),
+          Text('Girls that code'),
+          ],
+      ),
+    );
+  }
+}

23948f13b9a435efca53f37b3a603a503f3eb7ed
anu <mercyjae20@gmail.com>
2023-01-12 10:17:33 -0800

    feat: implement login UI

---
+++ lib/login_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LogInScreen extends StatelessWidget {
+  const LogInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(children: [
+      Text("Girls that code"),
+            Text("Girls that code"),
+                  Text("Girls that code"),
+                        Text("Girls that code"),
+                              Text("Girls that code"),
+    ]),);
+  }
+}
